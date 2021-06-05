dda1ab0ee7aaec31f86952442143148f954df4be
Joe Kim <jkjundo@yahoo.com>
2021-06-05 01:53:35 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'facebook': {
+        clientId: '1148350372309170',
+        clientSecret: 'a49dea7f28a117b56a91e0e915142eb5'
 }
